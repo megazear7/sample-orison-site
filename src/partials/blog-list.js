71adb8dbe3bc25c92e9277fa3ab2c72c617e7562
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-05-16 09:12:11 -0400

    Display tweaks to mobile blog and product listings.

--- src/partials/blog-list.js
+++ src/partials/blog-list.js
@@ -4,7 +4,15 @@ export default async blogs => html`
   ${blogs.map(blog => html`
     <section class="blog-overview">
       <h2>${blog.fields.title}</h2>
-      <div class="small-overview">
+      <img class="mobile"
+           alt="${blog.fields.desktopHeroImage.fields.description}"
+           src="${blog.fields.desktopHeroImage.fields.file.url}?w=400">
+      <p class="mobile">
+        ${blog.fields.description}
+        <br>
+        <a href="/blog/${blog.fields.slug}.html">Read More</a>
+      </p>
+      <div class="small-overview desktop-flex">
         <img alt="${blog.fields.desktopHeroImage.fields.description}"
              src="${blog.fields.desktopHeroImage.fields.file.url}?w=400">
          <p>
--- src/partials/product-list.js
+++ src/partials/product-list.js
@@ -7,7 +7,15 @@ export default async (products, disabled) => html`
         ${product.fields.title}
         <small>${'$' + product.fields.price}</small>
       </h2>
-      <div class="small-overview">
+      <img class="mobile"
+           alt="${product.fields.desktopHeroImage.fields.description}"
+           src="${product.fields.desktopHeroImage.fields.file.url}?w=400">
+      <p class="mobile">
+        ${product.fields.description}
+        <br>
+        <a href="/products/${product.fields.slug}.html">Read More</a>
+      </p>
+      <div class="small-overview desktop-flex">
         <div>
           <img alt="${product.fields.desktopHeroImage.fields.description}"
                src="${product.fields.desktopHeroImage.fields.file.url}?w=400">
